87914a1e791f347e2dac3a2f4fa92bec60c50f29
Papri Saha <paprisaha1999@gmail.com>
2023-01-20 18:32:47 +0530

    day-4

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import CarsPage from "../Pages/CarsPage/CarsPage";
 import SingleCarsPage from "../Pages/CarsPage/SingleCarsPage";
-import Home from "../Pages/Home";
-import SignupCard from "./Signuppage";
-import Loginpage from "./Loginpage";
+import Home from "../Pages/HomePage/Home";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<SignupCard />} />
-      <Route path="/login" element={<Loginpage />} />
       <Route path="/cars" element={<CarsPage />} />
       <Route path="/cars/:id" element={<SingleCarsPage />} />
+      <Route path="/payment" element={<SingleCarsPage />} />
     </Routes>
   );
 };
