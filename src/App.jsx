9721ff39a3bd98814dccda91d9918b38a00ed082
Papri Saha <paprisaha1999@gmail.com>
2023-01-20 10:23:14 +0530

    day3

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
 import CarsPage from "./Pages/CarsPage";
 import Signuppage from "./Components/Signuppage";
-import Loginpage from "./Components/Loginpage";
-import Navbar from "./Components/Navbar";
-
 function App() {
   return (
     <div className="App">
       <Signuppage />
       <AllRoutes />
-      <Loginpage />
     </div>
   );
 }
