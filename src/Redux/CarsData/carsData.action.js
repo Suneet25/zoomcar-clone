9a8c05c4087cf816a39650fcbe19638441f0b2a0
Papri Saha <paprisaha1999@gmail.com>
2023-01-20 10:16:37 +0530

    day-3

--- src/Redux/CarsData/carsData.action.js
+++ src/Redux/CarsData/carsData.action.js
@@ -1,16 +1,32 @@
-// import axios from "axios";
-// import {
-//   CARSDATA_GET_ERROR,
-//   CARSDATA_GET_LOADING,
-//   CARSDATA_GET_SUCCESS,
-// } from "./carsData.types";
+import axios from "axios";
+import {
+  CARSDATA_GET_ERROR,
+  CARSDATA_GET_LOADING,
+  CARSDATA_GET_SUCCESS,
+} from "./carsData.types";
 
-// export const getCarData = () => async (dispatch) => {
+export const getCarData = () => async (dispatch) => {
+  /* Here we are showing loading thing */
+  dispatch({ type: CARSDATA_GET_LOADING });
+  try {
+    let data = await axios.get(
+      `http://localhost:8080/cars`,
+      // `https://backend-by-nitin.onrender.com/api/cars`
+    );
+    data = await data.data;
+    dispatch({ type: CARSDATA_GET_SUCCESS, payload: data });
+    // console.log("data in action page", data);
+  } catch (error) {
+    dispatch({ type: CARSDATA_GET_ERROR });
+  }
+};
+
+// export const getCarDataASC = () => async (dispatch) => {
 //   /* Here we are showing loading thing */
 //   dispatch({ type: CARSDATA_GET_LOADING });
 //   try {
 //     let data = await axios.get(
-//       `http://localhost:8080/cars`,
+//       ``
 //       // `https://backend-by-nitin.onrender.com/api/cars`
 //     );
 //     data = await data.data;
@@ -20,19 +36,3 @@
 //     dispatch({ type: CARSDATA_GET_ERROR });
 //   }
 // };
-
-// // export const getCarDataASC = () => async (dispatch) => {
-// //   /* Here we are showing loading thing */
-// //   dispatch({ type: CARSDATA_GET_LOADING });
-// //   try {
-// //     let data = await axios.get(
-// //       ``
-// //       // `https://backend-by-nitin.onrender.com/api/cars`
-// //     );
-// //     data = await data.data;
-// //     dispatch({ type: CARSDATA_GET_SUCCESS, payload: data });
-// //     // console.log("data in action page", data);
-// //   } catch (error) {
-// //     dispatch({ type: CARSDATA_GET_ERROR });
-// //   }
-// // };
--- src/Redux/CarsData/carsData.reducer.js
+++ src/Redux/CarsData/carsData.reducer.js
@@ -1,46 +1,46 @@
-// import {
-//   CARSDATA_GET_ERROR,
-//   CARSDATA_GET_LOADING,
-//   CARSDATA_GET_SUCCESS,
-// } from "./carsData.types";
+import {
+  CARSDATA_GET_ERROR,
+  CARSDATA_GET_LOADING,
+  CARSDATA_GET_SUCCESS,
+} from "./carsData.types";
 
-// const initialState = {
-//   loading: false,
-//   error: false,
-//   data: [],
-// };
+const initialState = {
+  loading: false,
+  error: false,
+  data: [],
+};
 
-// export const CarsPageReducer = (state = initialState, { type, payload }) => {
-//   switch (type) {
-//     case CARSDATA_GET_LOADING: {
-//       return {
-//         ...state,
-//         loading: true,
-//       };
-//     }
-//     case CARSDATA_GET_SUCCESS: {
-//       return {
-//         ...state,
-//         loading: false,
-//         data: payload,
-//       };
-//     }
-//     case CARSDATA_GET_ERROR: {
-//       return {
-//         ...state,
-//         loading: false,
-//         error: true,
-//       };
-//     }
+export const CarsPageReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case CARSDATA_GET_LOADING: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case CARSDATA_GET_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        data: payload,
+      };
+    }
+    case CARSDATA_GET_ERROR: {
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+    }
 
-//     /* in Reset case we are returning InitialState */
-//     // case FEEDS_RESET: {
-//     //   return {
-//     //     ...initialState,
-//     //   };
-//     // }
-//     default: {
-//       return state;
-//     }
-//   }
-// };
+    /* in Reset case we are returning InitialState */
+    // case FEEDS_RESET: {
+    //   return {
+    //     ...initialState,
+    //   };
+    // }
+    default: {
+      return state;
+    }
+  }
+};
